b158e375f92b7c3398702e9fed37050ed26de5b4
yousseframy24 <youssef.r@bronto24.com>
2023-08-30 07:46:39 +0300

    adding pages

--- src/app/number-speed-accuracy/page.tsx
+++ src/app/number-speed-accuracy/page.tsx
@@ -3,13 +3,13 @@ import { QuestionType } from "../api/number-speed-accuracy/route";
 import { shuffle } from "@/lib/shuffle";
 
 export default async function NumberSpeedAccuracyPage() {
-  // const NumberData: Array<QuestionType> = await fetch(
-  //   `${process.env.URL}/api/number-speed-accuracy`,
-  // ).then((res) => res.json());
+  const NumberData: Array<QuestionType> = await fetch(
+    `${process.env.URL}/api/number-speed-accuracy`,
+  ).then((res) => res.json());
 
   return (
     <div className="flex min-h-screen w-full flex-col items-center justify-center gap-4 bg-violet-400">
-      {/* <MultipleQuestion multipleQuestions={shuffle(NumberData)} /> */}
+      <MultipleQuestion multipleQuestions={shuffle(NumberData)} />
     </div>
   );
 }
